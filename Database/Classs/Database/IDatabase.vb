2f553a3a2e175b8b5f9d81dc761bd5a65a269b55
Yogy Pratama <yogypr03@gmail.com>
2021-12-30 14:21:20 +0700

    Edited Interface

--- Database/Classs/Database/IDatabase.vb
+++ Database/Classs/Database/IDatabase.vb
@@ -1,8 +1,8 @@
 ﻿Public Interface IDatabase
 
-    Function DBSelect(param As String, table As String, where As String) As DataTable
-    Sub DBInsert(param As String, table As String, values As String)
-    Sub DBUpdate(param As String, table As String, where As String)
+    Function DBSelect(table As String, param As String, where As String) As DataTable
+    Sub DBInsert(table As String, param As String, values As String)
+    Sub DBUpdate(table As String, param As String, where As String)
     Sub DBDelete(table As String, where As String)
     Function GetDate(_date As Date) As String
     Sub FolderExist(path As String)
