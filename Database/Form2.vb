f1612e636b90d8157d86d2dfd50dff7cc6933537
irgajulian <diputra184@gmail.com>
2022-01-10 10:58:07 +0700

    test 2 frorm
    test 2 form accessing same connections

--- Database/Form2.vb
+++ Database/Form2.vb
@@ -2,20 +2,17 @@
 Imports System.Threading
 Imports Database.DBManager
 Imports Database.SQLite
-Public Class Form1
-    Private _testSQ As IDBManager = New DBManager()
-    Private a As SQLite = New SQLite()
+Imports Database.Form1
+Public Class Form2
+
+    Public Property _testSQ As DBManager
+    Public Property a As SQLite ' = New SQLite()
     Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
         '_testSQ.GetDataBase("usersDb", TextBox1.Text, "-sqlite")
         a = _testSQ.GetDataBase("PRODUCT", "1", "-sqlite")
 
     End Sub
     Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
-        ' a.AddProduction("123123321", "TESTPRODUCTXV", 200, 30, "Hermina")
-        Dim form2 As New Form2
-        form2._testSQ = _testSQ
-        form2.a = a
-        Me.Hide()
-        form2.Show()
+        a.AddProduction("123123321", "TESTPRODUCTXV", 200, 30, "Hermina")
     End Sub
 End Class
